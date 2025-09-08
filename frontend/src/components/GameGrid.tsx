5c5b1a552ec27fe331129e5c81a77698e5f8015c
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-09-08 12:27:03 +0200

    Simple Grid

--- frontend/src/components/GameGrid.tsx
+++ frontend/src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import {useGames} from "../hooks/useGames";
-import {Text} from "@chakra-ui/react";
+import {SimpleGrid, Text} from "@chakra-ui/react";
+import { GameCard } from "./GameCard";
 
 
 export const GameGrid = () => {
@@ -8,12 +9,14 @@ export const GameGrid = () => {
 
   return (
     <>
-      <div>{error && <Text>{error}</Text>}</div>
-      <ul>
-        {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
-        ))}
-      </ul>
+      <div>
+        {error && <Text>{error}</Text>}
+        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} padding={2}>
+          {games.map((game) => (
+            <GameCard key={game.id} game={game}/>
+          ))}
+        </SimpleGrid>
+      </div>
     </>
   );
 }
